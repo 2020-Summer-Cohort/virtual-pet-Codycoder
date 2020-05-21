9458f4b2c4b8ee8fce5e66758d711e80dc923fdb
Codycoder <andykohler99@gmail.com>
2020-05-20 22:18:30 -0400

    First iteration done.

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -6,5 +6,88 @@ namespace VirtualPet
 {
     public class Pet
     {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Hunger { get; set; }
+        public int Boredom { get; set; }
+        public int Health { get; set; }
+
+
+        public Pet(string name, string species)
+        {
+            Name = name;
+            Species = species;
+        }
+
+        public Pet()
+        {
+            Hunger = 50;
+            Boredom = 60;
+            Health = 30;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetSpecies(string species)
+        {
+            Species = species;
+        }
+
+        public string GetSpecies()
+        {
+            return Species;
+        }
+
+        public int GetHunger()
+        {
+            return Hunger;
+        }
+
+        public int GetBoredom()
+        {
+            return Boredom;
+        }
+
+        public int GetHealth()
+        {
+            return Health;
+        }
+
+        public void Feed()
+        {
+            Hunger -= 40;
+            Console.WriteLine("You have fed your pet.");
+        }
+
+        public void SeeDoctor()
+        {
+            Health += 30;
+            Console.WriteLine("You have taken your pet to the doctor.");
+        }
+
+        public void Play()
+        {
+            Hunger += 10;
+            Boredom -= 20;
+            Health += 10;
+
+            Console.WriteLine("You have played with your pet.");
+
+        }
+
+        public void Tick()
+        {
+            Hunger += 5;
+            Boredom += 5;
+            Health -= 5;
+        }
     }
 }
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
+using System.Transactions;
 
 namespace VirtualPet
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Welcome to Virtual Pets");
+
+            Pet Pet = new Pet();
+
+            Console.WriteLine("Hello! Welcome to Virtual Pets.\n");
+
+            Console.WriteLine("What's the name of your new pet?");
+            Pet.SetName(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("What's the species of your new pet?");
+            Pet.SetSpecies(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine($"\n{Pet.GetName()} the {Pet.GetSpecies()} is here!");
+
+            bool gamePlay = true;
+            do
+            {
+                ;
+                Console.WriteLine($"Hunger is {Pet.GetHunger()}.");
+                Console.WriteLine($"Boredom is {Pet.GetBoredom()}.");
+                Console.WriteLine($"Health is {Pet.GetHealth()}."); 
+                Pet.Tick();
+                Console.WriteLine("\nWhat would you like to do with your pet?\n");
+
+                Console.WriteLine("Play with my pet - press 1");
+                Console.WriteLine("\nFeed my pet - press 2");
+                Console.WriteLine("\nTake my pet to the doctor - press 3");
+                Console.WriteLine("\nQuit - press 4");
+
+                string inPlay = Console.ReadLine();
+                Console.Clear();
+
+                switch (inPlay)
+
+                {
+                    case "1":
+                        Pet.Play();
+                        break;
+
+                    case "2":
+                        Pet.Feed();
+                        break;
+
+                    case "3":
+                        Pet.SeeDoctor();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Thanks for playing!");
+                            break;
+
+                    default:
+                        Console.WriteLine("Enter a number.");
+                        break;
+
+                }
+
+
+            } while (gamePlay);
+
         }
     }
 }
